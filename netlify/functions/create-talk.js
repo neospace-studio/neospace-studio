466d9d3b585162f0da78a5be1714e6fd3d581901
neospace-studio <maoamaan@gmail.com>
2026-03-26 14:49:11 +0400

    Update create-talk.js

--- netlify/functions/create-talk.js
+++ netlify/functions/create-talk.js
@@ -1,5 +1,4 @@
-// D-ID API key is already in correct format: base64user:password
-const DID_API_KEY = 'bWFvYW1hYW5AZ21haWwuY29t:IveqbttS8H5F-a5PGBqXm';
+const DID_API_KEY = 'bWFvYW1hYW5AZ21haWwuY29t:RMgkc1QkKRJGs1mHOok4D';
 
 exports.handler = async (event) => {
   const headers = {
